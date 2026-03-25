67794bf42b60ab25232f46a5f50c01646660bf46
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-25 16:00:59 -0400

    Project Initialization
    -Organized the file structure for the project
    -Added Patient class and intialized it in main.py

---
+++ src/records-program/main.py
@@ -0,0 +1,4 @@
+from patient import Patient
+from datetime import datetime
+
+patients = [Patient("Nimmi", "JingleBop", datetime(1990,1,6), 185, 90.3, True)]
---
+++ src/records-program/patient.py
@@ -0,0 +1,9 @@
+class Patient:
+    
+    def __init__(self, first_name, last_name, date_of_birth, height, weight, taking_meds):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.height = height
+        self.weight = weight
+        self.taking_meds = taking_meds
