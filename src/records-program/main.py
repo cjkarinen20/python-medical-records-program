3f6f516962b8375257cc354ae27340379aec2826
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-25 16:29:21 -0400

    Convert Patients Strings to a List of Data
    -Create a function to populate a list with
    patient data.
    -Changed the print statement to use the output of
    the new method.
    -Installed and imported PySimpleGUI.

--- src/records-program/main.py
+++ src/records-program/main.py
@@ -1,8 +1,15 @@
 from patient import Patient
 from datetime import datetime
+import PySimpleGUI as sg
 
 patients = [Patient("Nimmi", "JingleBop", datetime(1,6,1990), 185, 90.3, True),
            Patient("Joe", "Blough", datetime(7,2,2000), 185, 90.3, False),
            Patient("Elfrick", "Belghini", datetime(6,25,1962), 185, 90.3, False)]
+def convert_patients_to_table_data():
+    patient_data = []
+    for patient in patients:
+        strings = patient.convert_info_to_strings()
+        patient_data.append(strings)
+    return patient_data
 
-print(patients[0].convert_info_to_strings())
+print(convert_patients_to_table_data())
