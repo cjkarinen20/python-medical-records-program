2ff69fe95568119358600fe3b07eeecd52536f78
cjkarinen20 <cjkarinen20@gmail.com>
2026-03-25 16:12:59 -0400

    Print Patient Information
    -Created a function to convert patient info into
    printable strings.
    -Added more example patients to list in main.
    -Printed the first patient in the list, using the
    the new conversion function.

--- src/records-program/main.py
+++ src/records-program/main.py
@@ -1,4 +1,8 @@
 from patient import Patient
 from datetime import datetime
 
-patients = [Patient("Nimmi", "JingleBop", datetime(1990,1,6), 185, 90.3, True)]
+patients = [Patient("Nimmi", "JingleBop", datetime(1,6,1990), 185, 90.3, True),
+           Patient("Joe", "Blough", datetime(7,2,2000), 185, 90.3, False),
+           Patient("Elfrick", "Belghini", datetime(6,25,1962), 185, 90.3, False)]
+
+print(patients[0].convert_info_to_strings())
--- src/records-program/patient.py
+++ src/records-program/patient.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Patient:
     
     def __init__(self, first_name, last_name, date_of_birth, height, weight, taking_meds):
@@ -7,3 +9,11 @@ class Patient:
         self.height = height
         self.weight = weight
         self.taking_meds = taking_meds
+        
+    def convert_info_to_strings(self):
+        date_of_birth = datetime.strftime(self.date_of_birth, '%m/%d/%Y') #MM/DD/YYYY
+        height = str(self.height)
+        weight = str(self.weight)
+        taking_meds = str(self.taking_meds)
+        
+        return [self.first_name, self.last_name, date_of_birth, height, weight, taking_meds]
