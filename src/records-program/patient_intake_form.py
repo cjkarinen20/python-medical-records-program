3cbfd50483001f82a124106f377cd79dc11e8e81
cjkarinen20 <cjkarinen20@gmail.com>
2026-04-01 18:29:55 -0400

    Error Fixes
    -Fixed a TypeError and a KeyError that occur when
    adding a new patient via the form.

--- src/records-program/patient_intake_form.py
+++ src/records-program/patient_intake_form.py
@@ -6,7 +6,7 @@ def read_input_values(values):
     date_of_birth = values["DATE_OF_BIRTH"]
     height = values["HEIGHT"]
     weight = values["WEIGHT"]
-    taking_medication = values["IS_TAKING_MEDICATIONS"]
+    taking_medication = values["IS_TAKING_MEDICATION"]
     print(first_name)
     print(last_name)
     print(date_of_birth)
@@ -20,9 +20,9 @@ def create_layout():
     [sg.Text("First Name"), sg.Input(key = "FIRST_NAME")],
     [sg.Text("Last Name"), sg.Input(key = "LAST_NAME")],
     [sg.Text("Date Of Birth"), sg.Input(key = "DATE_OF_BIRTH"), 
-    sg.CalendarButton("Select Date", format = '%Y/%m/%d')],
-    [sg.Text("Height"), sg.Input(key = "HEIGHT")]
-    [sg.Text("Weight"), sg.Input(key = "WEIGHT")]
+    sg.CalendarButton("Select Date", format = '%m/%d/%Y')],
+    [sg.Text("Height"), sg.Input(key = "HEIGHT")],
+    [sg.Text("Weight"), sg.Input(key = "WEIGHT")],
     [sg.Text("Is Taking Medication?"), sg.Checkbox("Yes", key = "IS_TAKING_MEDICATION")],
     [sg.Cancel(), sg.Button("Save")]]
 
